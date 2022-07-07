6d8fe2a22691b650c1988b3f1082f8535c76dd01
cuibaoqiang <cuibaoqiang@qq.com>
2022-07-07 08:45:14 +0800

    druid 监控

--- spring-druid/src/main/java/org/spring/demo/springdruid/config/MyDataSourceConfig.java
+++ spring-druid/src/main/java/org/spring/demo/springdruid/config/MyDataSourceConfig.java
@@ -2,12 +2,16 @@ package org.spring.demo.springdruid.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
 import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
+import java.util.Arrays;
+import java.util.Collections;
 
 /**
  * @author cuibaoqiang
@@ -17,6 +21,10 @@ import javax.sql.DataSource;
 @Configuration
 public class MyDataSourceConfig {
 
+    /**
+     * 根据spring.datasource 前缀设置Druid连接属性
+     * @return
+     */
     @Bean
     @ConfigurationProperties(prefix = "spring.datasource")
     public DataSource dataSource() {
@@ -24,10 +32,28 @@ public class MyDataSourceConfig {
         return dataSource;
     }
 
+    /**
+     * 开启Druid监控页面
+     * @return
+     */
     @Bean
     public ServletRegistrationBean registrationBean() {
         StatViewServlet statViewServlet = new StatViewServlet();
         ServletRegistrationBean<StatViewServlet> registrationBean = new ServletRegistrationBean<StatViewServlet>(statViewServlet, "/druid/*");
+        registrationBean.addInitParameter("loginUsername","root");
+        registrationBean.addInitParameter("loginPassword","root");
         return registrationBean;
+    }
+
+    /**
+     * 开启druid web应用uri监控
+     */
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        WebStatFilter webStatFilter = new WebStatFilter();
+        FilterRegistrationBean<WebStatFilter> webStatFilterFilterRegistrationBean = new FilterRegistrationBean<>(webStatFilter);
+        webStatFilterFilterRegistrationBean.setUrlPatterns(Collections.singletonList("/*"));
+        webStatFilterFilterRegistrationBean.addInitParameter("exclusions","*.html,*.js,*.css,*.json");
+        return webStatFilterFilterRegistrationBean;
     }
 }
