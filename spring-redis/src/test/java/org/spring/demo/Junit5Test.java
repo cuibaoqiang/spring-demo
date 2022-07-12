4cc3f49a7457d8fdebcd8e411d8bc4d4e5e1c745
cuibaoqiang <cuibaoqiang@qq.com>
2022-07-12 08:38:57 +0800

    junit

--- spring-redis/src/test/java/org/spring/demo/Junit5Test.java
+++ spring-redis/src/test/java/org/spring/demo/Junit5Test.java
@@ -1,10 +1,18 @@
 package org.spring.demo;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.platform.commons.util.StringUtils;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.Duration;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.*;
 
 /**
  * @author cuibaoqiang
@@ -15,11 +23,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @DisplayName("测试")
 public class Junit5Test {
 
-    @Test
-    @DisplayName("Test")
-    public void test() {
-        System.out.println(1);
-    }
 
     @BeforeEach
     void beforeEach() {
@@ -29,5 +32,108 @@ public class Junit5Test {
     @AfterEach
     void afterEach() {
         System.out.println("afterEach");
+    }
+
+    @Test
+    @DisplayName("Test1")
+    public void test() {
+        System.out.println(1);
+    }
+
+
+    @Test
+    @DisplayName("simple assertion")
+    public void simple() {
+        assertEquals(3, 1 + 2, "simple math");
+        assertNotEquals(3, 1 + 1);
+
+        assertNotSame(new Object(), new Object());
+        Object obj = new Object();
+        assertSame(obj, obj);
+
+        assertFalse(1 > 2);
+        assertTrue(1 < 2);
+
+        assertNull(null);
+        assertNotNull(new Object());
+    }
+
+
+    @Test
+    @DisplayName("array assertion")
+    public void array() {
+        assertArrayEquals(new int[]{1, 2}, new int[] {1, 2});
+    }
+
+    @Test
+    @DisplayName("assert all")
+    public void all() {
+        assertAll("Math",
+                () -> assertEquals(2, 1 + 1),
+                () -> assertTrue(1 > 0)
+        );
+    }
+
+    @Test
+    @DisplayName("异常测试")
+    public void exceptionTest() {
+        ArithmeticException exception = Assertions.assertThrows(
+                //扔出断言异常
+                ArithmeticException.class, () -> System.out.println(1 % 0));
+
+    }
+
+    @Test
+    @DisplayName("超时测试")
+    public void timeoutTest() {
+        //如果测试方法时间超过1s将会异常
+        Assertions.assertTimeout(Duration.ofMillis(1000), () -> Thread.sleep(500));
+    }
+
+    @Test
+    @DisplayName("fail")
+    public void shouldFail() {
+        fail("This should fail");
+    }
+
+    @DisplayName("前置条件")
+    public class AssumptionsTest {
+        private final String environment = "DEV";
+
+        @Test
+        @DisplayName("simple")
+        public void simpleAssume() {
+            assumeTrue(Objects.equals(this.environment, "DEV"));
+            assumeFalse(() -> Objects.equals(this.environment, "PROD"));
+        }
+
+        @Test
+        @DisplayName("assume then do")
+        public void assumeThenDo() {
+            assumingThat(
+                    Objects.equals(this.environment, "DEV"),
+                    () -> System.out.println("In DEV")
+            );
+        }
+    }
+    @ParameterizedTest
+    @ValueSource(strings = {"one", "two", "three"})
+    @DisplayName("参数化测试1")
+    public void parameterizedTest1(String string) {
+        System.out.println(string);
+        Assertions.assertTrue(StringUtils.isNotBlank(string));
+    }
+
+
+    @ParameterizedTest
+    @MethodSource("method")    //指定方法名
+    @DisplayName("方法来源参数")
+    public void testWithExplicitLocalMethodSource(String name) {
+        System.out.println(name);
+        Assertions.assertNotNull(name);
+    }
+
+    static Stream<String> method() {
+        return Stream.of("apple", "banana");
     }
 }
