1cfa45a25d578a47201a99fbc35bce95ae975eb8
cuibaoqiang <cuibaoqiang@qq.com>
2022-07-13 23:17:50 +0800

    监控服务

---
+++ spring-admin-server/src/main/java/org/spring/demo/SpringBootStarter.java
@@ -0,0 +1,15 @@
+package org.spring.demo;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableAdminServer
+@SpringBootApplication
+public class SpringBootStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStarter.class, args);
+    }
+
+}
